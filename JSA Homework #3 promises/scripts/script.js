2c479eba7c4683b8153f6ce51a0b3365b62ebe71
Dejan <djovanov92@gmail.com>
2020-02-11 20:07:24 +0100

    sintaksikca korekcija i funkcionalen primer za branchin i pull request

--- JSA Homework #3 promises/scripts/script.js
+++ JSA Homework #3 promises/scripts/script.js
@@ -1,10 +1,10 @@
-// Task 1
+// // Task 1
 
-// testNumber = num1 => {  
+// testNumber = num1 => {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
 //             if (num1 > 10) {
-//                 resolve('Success!')        
+//                 resolve('Success!')
 //             } else {
 //                 reject('ERROR: The number is below 10!')
 //             }
@@ -13,98 +13,98 @@
 // }
 
 // testNumber(15)
-// .then(success => console.log(success))
-// .catch(err => console.log(err))
-// .finally(() => console.log(`Everything finished in ${Math.round(performance.now()) / 1000}s`));
+//     .then(success => console.log(success))
+//     .catch(err => console.log(err))
+//     .finally(() => console.log(`Everything finished in ${Math.round(performance.now()) / 1000}s`));
 
-// Task 2 - 1
+// // Task 2 - 1
 
 // let arrayOfWords = ['button', 'erased', 'c#', 'darwins game', 'array'];
 
-// stringChecker = array => {
+// const stringChecker = array => {
 //     for (const word of array) {
-//         if (typeof word != 'string') {
+//         if (typeof word !== 'string') {
 //             return false;
 //         }
 //     }
 //     return true;
 // }
 
-// changeAllToCaps = array => {
+// const changeAllToCaps = array => {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             if (stringChecker(array) === true) {
-//                 resolve('Success! The array has been converted to upper case!')
+//             if (stringChecker(array)) {
 //                 let i = 0;
 //                 for (const word of array) {
-//                     array[i] = word.toUpperCase()
-//                         i++
+//                     array[i] = word.toUpperCase();
+//                     i++;
 //                 }
+//                 resolve('Success! The array has been converted to upper case!');
 //             } else {
-//                 reject(`ERROR: There is something else other than a string in the array!`)
+//                 reject(`ERROR: There is something else other than a string in the array!`);
 //             }
 //         }, 2000);
 //     })
 // }
 
 // changeAllToCaps(arrayOfWords)
-// .then(success => {
-//     console.log(success, arrayOfWords);
-// })
-// .catch(err => console.log(err))
-// .finally(() => console.log(`Everything finished in ${Math.round(performance.now()) / 1000}s`))
+//     .then(success => {
+//         console.log(success, arrayOfWords);
+//     })
+//     .catch(err => console.log(err))
+//     .finally(() => console.log(`Everything finished in ${Math.round(performance.now()) / 1000}s`));
 
-// Task 2 - 2
+// // Task 2 - 2
 
 // let arrayOfWords = ['button', 'erased', 'c#', 'darwins game', 'array'];
 
 
-// arraySort = array => {
-//     let newArr = array.sort((a,b) => a.localeCompare(b));
+// const arraySort = array => {
+//     let newArr = array.sort((a, b) => a.localeCompare(b));
 //     return newArr;
 // }
 
-// arrayChecker = array => {
+// const arrayChecker = array => {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
 //             if (array.length <= 1) {
-//                 reject(`ERROR: There are not enought words to sort the array!`)
+//                 reject(`ERROR: There are not enough words to sort the array!`);
 //             } else {
-//                 resolve(`Success! The array has been sorted!`)
-//                 arraySort(array)
+//                 arraySort(array);
+//                 resolve(`Success! The array has been sorted!`);
 //             }
 //         }, 2000);
 //     })
 // }
 
 // arrayChecker(arrayOfWords)
-// .then(success => console.log(success, arrayOfWords))
-// .catch(err => console.log(err))
-// .finally(() => console.log(`Everything finished in ${Math.round(performance.now()) / 1000}s`));
+//     .then(success => console.log(success, arrayOfWords))
+//     .catch(err => console.log(err))
+//     .finally(() => console.log(`Everything finished in ${Math.round(performance.now()) / 1000}s`));
 
-// Task 2 - 3
+// // Task 2 - 3
 
 // let arrayOfWords = ['button', 'erased', 'c#', 'darwins game', 'array'];
 
-// stringChecker = array => {
+// const stringChecker = array => {
 //     for (const word of array) {
-//         if (typeof word != 'string') {
+//         if (typeof word !== 'string') {
 //             return false;
 //         }
 //     }
 //     return true;
 // }
 
-// changeAllToCaps = array => {
+// const changeAllToCaps = array => {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             if (stringChecker(array) === true) {
-//                 resolve('Success! The array has been converted to upper case!')
+//             if (stringChecker(array)) {
 //                 let i = 0;
 //                 for (const word of array) {
-//                     array[i] = word.toUpperCase()
-//                         i++
+//                     array[i] = word.toUpperCase();
+//                     i++;
 //                 }
+//                 resolve('Success! The array has been converted to upper case!');
 //             } else {
 //                 reject(`ERROR: There is something else other than a string in the array!`)
 //             }
@@ -112,86 +112,86 @@
 //     })
 // }
 
-// arraySort = array => {
-//     let newArr = array.sort((a,b) => a.localeCompare(b));
+// const arraySort = array => {
+//     let newArr = array.sort((a, b) => a.localeCompare(b));
 //     return newArr;
 // }
 
-// arrayChecker = array => {
+// const arrayChecker = array => {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
 //             if (array.length <= 1) {
-//                 reject(`ERROR: There are not enought words to sort the array!`)
+//                 reject(`ERROR: There are not enough words to sort the array!`);
 //             } else {
-//                 resolve(`Success! The array has been sorted!`)
-//                 arraySort(array)
+//                 arraySort(array);
+//                 resolve(`Success! The array has been sorted!`);
 //             }
 //         }, 2000);
 //     })
 // }
 
 // changeAllToCaps(arrayOfWords)
-// .then(success => {
-//     console.log(success)
-//     return arrayChecker(arrayOfWords)
-// })
-// .then(success => {
-//     console.log(success)
-//     console.log(arrayOfWords)
-// })
-// .catch(err => console.log(err))
-// .finally(() => console.log(`Everything finished in ${Math.round(performance.now()) / 1000}s`));
+//     .then(success => {
+//         console.log(success);
+//         return arrayChecker(arrayOfWords);
+//     })
+//     .then(success => {
+//         console.log(success);
+//         console.log(arrayOfWords);
+//     })
+//     .catch(err => console.log(err))
+//     .finally(() => console.log(`Everything finished in ${Math.round(performance.now()) / 1000}s`));
 
 
 // BONUS    
 
 let arrayOfWords = ['button', 'erased', 'c#', 'darwins game', 'array'];
 
-stringChecker = array => {
+const stringChecker = array => {
     for (const word of array) {
-        if (typeof word != 'string') {
+        if (typeof word !== 'string') {
             return false;
         }
     }
     return true;
 }
 
-changeAllToCaps = array => {
+const changeAllToCaps = array => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (stringChecker(array) === true) {
-                resolve('Success! The array has been converted to upper case!')
+            if (stringChecker(array)) {
                 let i = 0;
                 for (const word of array) {
-                    array[i] = word.toUpperCase()
-                        i++
+                    array[i] = word.toUpperCase();
+                    i++;
                 }
+                resolve('Success! The array has been converted to upper case!');
             } else {
-                reject(`ERROR: There is something else other than a string in the array!`)
+                reject(`ERROR: There is something else other than a string in the array!`);
             }
         }, 2000);
     })
 }
 
-arraySort = array => {
+const arraySort = array => {
     let newArr = array.sort((a,b) => a.localeCompare(b));
     return newArr;
 }
 
-arrayChecker = array => {
+const arrayChecker = array => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (array.length <= 1) {
-                reject(`ERROR: There are not enought words to sort the array!`)
+                reject(`ERROR: There are not enough words to sort the array!`)
             } else {
-                resolve(`Success! The array has been sorted!`)
-                arraySort(array)
+                arraySort(array);
+                resolve(`Success! The array has been sorted!`);
             }
         }, 2000);
     })
 }
 
-asyncFunc = async array =>  {
+const asyncFunc = async array =>  {
     try {
         let changeCaps = await changeAllToCaps(array);
         console.log(changeCaps);
